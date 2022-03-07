95abecf4a0b16ad7e3d7e22240a2f95b19049eb3
chev <MOULOUDBEL>
2022-03-07 13:40:58 +0100

    8 test

--- mypets/routes/web.php
+++ mypets/routes/web.php
@@ -1,6 +1,6 @@
 <?php
 
-use Illuminate\Support\Facades\Route; 
+use Illuminate\Support\Facades\Route;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,7 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-Route::resource('chemin_url_animal',\App\Http\Controllers\AnimalsController);
+
 
 Auth::routes();
 
